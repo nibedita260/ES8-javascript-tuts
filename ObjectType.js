3607e0eadc13c369f01106b9dfcde48023fb1799
nibedita260 <deepi.nibedita260@gmail.com>
2021-12-15 23:32:33 +0530

    [Nibedita]Added:ObjectType in Javascript Features Implemented

---
+++ ObjectType.js
@@ -0,0 +1,10 @@
+const student ={
+    firstName:'Kannan',
+    lastName:'Sudhakaran'
+ }
+ console.log(Object.entries(student))
+ console.log(Object.values(student))
+
+ const marks = [10,20,30,40]
+   console.log(Object.entries(marks))
+   console.log(Object.values(marks))
